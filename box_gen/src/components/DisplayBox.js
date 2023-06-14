58a1f229a39d5ed99fd4d26059d4f73230e0d48c
Autumn Han <autumn9894@gmail.com>
2023-06-14 13:05:29 -0700

    worked out syntax error with box styling

--- box_gen/src/components/DisplayBox.js
+++ box_gen/src/components/DisplayBox.js
@@ -5,11 +5,11 @@ const DisplayBox = ({boxes}) => {
         <div>
             {boxes.map( (box, index) => {
                 return (
-                    <div style={{backgroundColor: box.color, height: box.height, width: box.width} key={index}}></div>
+                    <div style={{backgroundColor: box.color, height: box.height, width: box.width}} key={index}></div>
                 )
             })}
         </div>
-    );
+    )
 }
 
 export default DisplayBox;
