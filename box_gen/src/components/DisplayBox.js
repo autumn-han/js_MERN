db187c5cbc9d2b53ca7c4918ce244e92d8c7a0e3
Autumn Han <autumn9894@gmail.com>
2023-06-14 12:52:19 -0700

    working on functionality

--- box_gen/src/components/DisplayBox.js
+++ box_gen/src/components/DisplayBox.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const DisplayBox = (props) => {
-    
+const DisplayBox = ({boxes}) => {
     return (
-        <div></div>
+        <div>
+            {boxes.map( (box, index) => {
+                return (
+                    <div style={{backgroundColor: box.color, height: box.height, width: box.width} key={index}}></div>
+                )
+            })}
+        </div>
     );
 }
+
+export default DisplayBox;
