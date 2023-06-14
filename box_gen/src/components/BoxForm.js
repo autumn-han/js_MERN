8b4a5d126960cdaf6581c93ffa2f28ba2048047e
Autumn Han <autumn9894@gmail.com>
2023-06-14 14:24:03 -0700

    all functionalities and bonus features working

--- box_gen/src/components/BoxForm.js
+++ box_gen/src/components/BoxForm.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 
 const BoxForm = ({boxes, setBoxes}) => {
     const [color, setColor] = useState("");
+    const [int, setInt] = useState(0);
     const formHandler = (e) => {
         e.preventDefault();
-        const newBox = {color: color, height: "100px", width: "100px", display: "flex", margin: "10px"};
+        const newBox = {color: color, height: int + "px", width: int + "px"};
         setBoxes([...boxes, newBox]);
         setColor("");
     };
@@ -12,6 +13,8 @@ const BoxForm = ({boxes, setBoxes}) => {
         <form onSubmit={ formHandler }>
             <label htmlFor="color">Color: </label>
             <input type="text" name="color" value={ color } onChange={ (e) => setColor(e.target.value) } />
+            <label htmlFor="dimension">Dimension: </label>
+            <input type="number" name="dimension" value={ int } onChange={ (e) => setInt(e.target.value) } />
             <button>Add</button>
         </form>
     );
