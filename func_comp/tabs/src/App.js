504db2e92e377f7e24a69563b2db358993d8a02b
Autumn Han <autumn9894@gmail.com>
2023-06-16 11:57:33 -0700

    working on storing content for each tab

--- func_comp/tabs/src/App.js
+++ func_comp/tabs/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import {useState} from 'react';
+import Tabs from './components/Tabs';
 
 function App() {
+  const [tabs, setTabs] = useState([{label:"Tab 1", content:"Hello, this is Tab 1"}, {label:"Tab 2", content:"This is Tab 2, checking in"}, {label:"Tab 3", content:"Finally, I'm Tab 3!"}]);
   return (
-    // write in functional component here
+    <Tabs tabs={tabs} setTabs={setTabs} />
   )
 }
 
--- func_comp/tabs/src/components/Tabs.js
+++ func_comp/tabs/src/components/Tabs.js
@@ -1,14 +1,27 @@
 import React, {useState} from 'react';
 
-const Tabs = () => {
-    const [tabs, setTabs] = useState([]);
+const Tabs = ({tabs, setTabs}) => {
     const [label, setLabel] = useState("");
     const [content, setContent] = useState("");
+    const tab = {label: label, content: content};
+    const changeContent = (e, index) => {
+        const filteredTabs = tabs.filter( (tab, kpIdx) => {
+            return kpIdx === index;
+        })
+        setTabs(filteredTabs);
+    }
     return (
         <div>
-            <button></button>
-            <button></button>
-            <button></button>
+            {tabs.map( (tab, index) => {
+                return (
+                    <div key={index}>
+                        <button onClick={ (e) => changeContent(e, index) }>{tab.label}</button>
+                    </div>
+                )
+            })}
+            <textarea>{tab.content}</textarea>
         </div>
     )
 }
+
+export default Tabs;
