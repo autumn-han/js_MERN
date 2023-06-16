e43a7f5615e8c9d68722de7c13fdd81b9bc1a0c3
Autumn Han <autumn9894@gmail.com>
2023-06-15 18:38:17 -0700

    changed app to use axios for fetching data

--- api/poke_api/src/components/CatchEmAll.js
+++ api/poke_api/src/components/CatchEmAll.js
@@ -1,11 +1,15 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const CatchEmAll = (props) => {
+
+const CatchEmAll = () => {
     const [pokemon, setPokemon] = useState([]);
     useEffect( () => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-            .then(response => response.json())
-            .then(response => setPokemon(response.results))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then(response => {setPokemon(response.data.results)})
+            .catch(err => {
+                console.log(err)})
+        console.log(pokemon)
     }, []);
     return (
         <div>
