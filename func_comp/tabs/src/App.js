e3060f2339c6ac618037d5ed90a88d53e52e38e9
Autumn Han <autumn9894@gmail.com>
2023-06-15 19:10:36 -0700

    initial commit

---
+++ func_comp/tabs/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    // write in functional component here
+  )
+}
+
+export default App;
---
+++ func_comp/tabs/src/components/Tabs.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Tabs = () => {
+    const [tabs, setTabs] = useState([]);
+    const [label, setLabel] = useState("");
+    const [content, setContent] = useState("");
+    return (
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
