8b231444c573fadda82d608fdeaae98cf9cbc903
Autumn Han <autumn9894@gmail.com>
2023-06-16 15:52:46 -0700

    routing from the url is working

--- routing/routing_prac/src/App.js
+++ routing/routing_prac/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
+import Integer from './components/Integer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:num" element={<Integer />} />
       </Routes>
     </BrowserRouter>
   );
