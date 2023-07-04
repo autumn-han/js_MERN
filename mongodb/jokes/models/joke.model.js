9a0ca373cd3b7d069db8664c5ceb27b2cf03d216
Autumn Han <autumn9894@gmail.com>
2023-07-04 10:59:05 -0700

    adding validations to schema

--- mongodb/jokes/models/joke.model.js
+++ mongodb/jokes/models/joke.model.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
     setup: {
-        type: String
+        type: String,
+        required: [true, "Setup required"],
+        minlength: [10, "Setup must be at least 10 characters long"]
     },
     punchline: {
         type: String
     }
 });
 
-const Joke = mongoose.model("Joke", JokeSchema);
+const Joke = mongoose.model("joke", JokeSchema);
 
 module.exports = Joke;
