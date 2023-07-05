0eea3985c2f2758befa62b751109c083319b05d0
Autumn Han <autumn9894@gmail.com>
2023-07-05 16:15:00 -0700

    installed cors in server

--- fullstack/myNewProject/server/server.js
+++ fullstack/myNewProject/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 8000;
 
+app.use(cors());
 const personRoutes = require('./routes/person.routes');
 personRoutes(app);
 // the above two lines can also be written in shorthand as: require(./routes/person.routes)(app)
