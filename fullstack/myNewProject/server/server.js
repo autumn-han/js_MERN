805b027357d785241462ff9b8d264c44a9e8d525
Autumn Han <autumn9894@gmail.com>
2023-07-05 14:49:40 -0700

    backend setup

--- fullstack/myNewProject/server/server.js
+++ fullstack/myNewProject/server/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const personRoutes = require('./routes/person.routes');
+personRoutes(app);
+// the above two lines can also be written in shorthand as: require(./routes/person.routes)(app)
+
 app.listen(port, () => console.log('Listening on port: ${port}'));
