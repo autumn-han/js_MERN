9c7e915a6c8f7255d1705a7d415f1c4b8b175b32
Autumn Han <autumn9894@gmail.com>
2023-07-06 12:13:47 -0700

    draft of product form

---
+++ fullstack/prodman/client/src/components/Product.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const ProductForm = () => {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [desc, setDesc] = useState("");
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('https://localhost:8000/api', {
+            title: title,
+            price: price,
+            desc: desc
+        })
+            .then(res => console.log(res.data))
+            .catch(err => console.log("Something went wrong"))
+    };
+    return (
+        <form>
+            <p>
+                <label>Title</label>
+                <input type="text" onChange={(e) => setTitle(e.target.value)}/>
+            </p>
+            <p>
+                <label>Price</label>
+                <input type="number" onChange={(e) => setPrice(e.target.value)}/>
+            </p>
+            <p>
+                <label>Description</label>
+                <input type="text" onChange={(e) => setDesc(e.target.value)}/>
+            </p>
+        </form>
+    )
+};
+
+export default ProductForm;
--- fullstack/prodman/server/server.js
+++ fullstack/prodman/server/server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const productRoutes = require('./routes/product.routes');
+productRoutes(app);
+
 app.listen(port, () => console.log('Listening on port: 8000'));
