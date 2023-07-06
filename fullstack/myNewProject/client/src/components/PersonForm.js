af174b3865b92476bb941996c861ec9274485c93
Autumn Han <autumn9894@gmail.com>
2023-07-06 10:53:39 -0700

    building out post form in PersonForm

--- fullstack/myNewProject/client/src/components/PersonForm.js
+++ fullstack/myNewProject/client/src/components/PersonForm.js
@@ -2,16 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const PersonForm = () => {
-    const [ message, setMessage ] = useState("Loading...")
-    useEffect( () => {
-        axios.get('http://localhost:8000/api')
-            .then( res => setMessage(res.data.message))
-            .catch( err => console.log("Something went wrong"))
-    }, []);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/people', {
+            firstName: firstName,
+            lastName: lastName
+        })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err => console.log(err))
+    }
     return (
-        <div>
-            <h2>Message from the backend: {message}</h2>
-        </div>
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>First Name</label>
+                <input type="text" onChange={(e) => setFirstName(e.target.value)}/>
+            </p>
+            <p>
+                <label>Last Name</label>
+                <input type="text" onChange={(e) => setLastName(e.target.value)}/>
+            </p>
+            <input type="submit"/>
+        </form>
     )
 };
 
