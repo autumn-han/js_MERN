65723a7044a9ed6a79bf86d7ebdd5da13ab4be3c
Autumn Han <autumn9894@gmail.com>
2023-07-12 11:58:26 -0700

    learning to reuse components

--- fullstack/myNewProject/client/src/views/Main.js
+++ fullstack/myNewProject/client/src/views/Main.js
@@ -1,20 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import PersonForm from "../components/PersonForm";
 import PersonList from "../components/PersonList";
 
 const Main = (props) => {
   const [people, setPeople] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/people")
+      .then((res) => {
+        setPeople(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   const removeFromDom = (personID) => {
-    setPeople(people.filter((person) => person._id != personID));
+    axios
+      .delete("http://localhost:8000/api/people/" + personID)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setPeople(people.filter((person) => person._id != personID));
+      })
+      .catch((err) => console.log(err));
+  };
+  const createPerson = (personParam) => {
+    axios
+      .post("http://localhost:8000/api/people", personParam)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setPeople([...people, res.data]);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div>
-      <PersonForm people={people} setPeople={setPeople} />
-      <PersonList
-        people={people}
-        setPeople={setPeople}
-        removeFromDom={removeFromDom}
+      <PersonForm
+        onSubmitProp={createPerson}
+        initialFirstName=""
+        initialLastName=""
       />
+      <PersonList people={people} removeFromDom={removeFromDom} />
     </div>
   );
 };
