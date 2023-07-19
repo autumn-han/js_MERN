9f7c1003e57de15c9933ceb815ee35deae1540c9
Autumn Han <autumn9894@gmail.com>
2023-07-19 12:31:29 -0700

    initial commit for team_man

---
+++ adv_mern/team_man/server/models/player.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PlayerSchema = new mongoose.Schema(
+  {
+    playerName: {
+      type: String,
+    },
+    games: [
+      {
+        game: 1,
+        status: "undecided",
+      },
+      {
+        game: 2,
+        status: "undecided",
+      },
+      {
+        game: 3,
+        status: "undecided",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Player", PlayerSchema);
